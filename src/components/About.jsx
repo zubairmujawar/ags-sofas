d61211cb12ae894f9628114f248256f4d19f4901
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2024-02-18 18:30:51 +0530

    services completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
       <div className=" absolute w-full h-full grid items-center gap-5 sm:grid-cols-10">
         <div className="sm:col-span-6" style={{paddingLeft: "2rem"}}>
           <h1 className="font-bold sm:text-5xl text-2xl p-5 self-start">About</h1>
-          <p className="sm:text-3xl text-xl p-4 sm:-2 text-black">
+          <p className="sm:text-2xl text-xl p-4 sm:-2 text-black">
           At the heart of our business lies a commitment to deliver excellence. We believe that the home is a sacred space, and each piece of furniture and every interior accessory should reflect the essence of the homeowner's personality and style. With this belief in mind, we strive to curate a diverse and exquisite collection that caters to various tastes, preferences, and lifestyles.
           </p>
           <button class="relative m-[5px] inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800" style={{marginLeft:"15px"}}>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -17,19 +17,21 @@ const Services = () => {
         className=" absolute w-full h-full grid items-center  sm:grid-cols-10"
         style={{ justifyItems: "center" }}
       >
-        <div className="sm:col-span-4" style={{ paddingLeft: "2rem" }}>
+        <div className="sm:col-span-4 flex flex-col items-start" style={{ paddingLeft: "2rem" }}>
           <h1 className="font-bold sm:text-5xl text-2xl p-5 self-start">
-            Services
+            MODERN FURNITURE
           </h1>
-          <p className="sm:text-3xl text-sm p-4 sm:-2 text-black">
-            At the heart of our business lies a commitment to deliver
-            excellence. We believe that the home is a sacred space, and each
-            piece of furniture and every interior accessory should reflect the
-            essence of the homeowner's personality and style. With this belief
-            in mind, we strive to curate a diverse and exquisite collection that
-            caters to various tastes, preferences, and lifestyles.
+          <h2 className="collection">Collections</h2>
+          <p className="sm:text-xl text-sm p-4 sm:-2 text-black">
+            Explore our carefully curated modern furniture collection designed
+            to elevate your living spaces. Discover a blend of sleek designs,
+            premium materials, and timeless craftsmanship that perfectly
+            balances aesthetics and functionality.
           </p>
-          <button class="relative m-[5px] inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800" style={{marginLeft:"15px"}}>
+          <button
+            class="relative m-[5px] inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800"
+            style={{ marginLeft: "15px" }}
+          >
             <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-700 rounded-md group-hover:bg-opacity-0">
               shop now
             </span>
