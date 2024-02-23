d56b861d8a43c77be60ae78470b0981b40771bb1
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2024-02-23 21:19:13 +0530

    changes in css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import aboutChair from "../assets/about-chair.png";
 
 const About = () => {
   return (
-    <div className=" h-[120vh] sm:h-[100vh] flex justify-center" >
+    <div className=" h-[120vh] sm:h-[100vh] w-full flex justify-center" >
       {/* <img
         src={aboutImg}
         alt=" home-page-img"
@@ -15,19 +15,22 @@ const About = () => {
       <div className=" absolute w-full h-full grid items-center gap-5 sm:grid-cols-10">
         <div className="sm:col-span-6" style={{paddingLeft: "2rem"}}>
         
-          <h1 className="font-bold sm:text-5xl text-2xl p-5 self-start">About</h1>
+          <h1 className="font-bold sm:text-5xl m-3 text-2xl p-5 self-start">About</h1>
           <p className="sm:text-2xl text-xl p-4 sm:-2 text-black">
           At the heart of our business lies a commitment to deliver excellence. We believe that the home is a sacred space, and each piece of furniture and every interior accessory should reflect the essence of the homeowner's personality and style. With this belief in mind, we strive to curate a diverse and exquisite collection that caters to various tastes, preferences, and lifestyles.
           </p>
-          <button class="relative m-[5px] inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800" style={{marginLeft:"15px"}}>
-            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-700 rounded-md group-hover:bg-opacity-0">
-              more info
-            </span>
-          </button>
-          <div className="animation-div"></div>
+          <button
+              class="relative m-[5px] inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800"
+              style={{ marginLeft: "15px" }}
+            >
+              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-700 rounded-md group-hover:bg-opacity-0">
+                more info
+              </span>
+            </button>
+          {/* <div className="animation-div"></div> */}
         </div>
         <div className=" min-h-[200px] py-4 sm:w-[28rem] sm:px-5 sm:col-span-4">
-          <img src={aboutChair} alt="aboutChairImg"  className="sm:w-full"/>  
+          <img src={aboutChair} alt="aboutChairImg"  className="sm:w-full w-[80%]"/>  
         </div>
           <div className="animation-div-right"></div>
       </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
         </div>
       </div>
       {/* About page */}
-      <div className=" h-[110vh] sm:h-[100vh] flex justify-center">
+      <div className=" h-[105vh] sm:h-[100vh] flex justify-center">
         {/* <img
         src={aboutImg}
         alt=" home-page-img"
@@ -54,58 +54,60 @@ const Home = () => {
             </button>
           </div>
           <div className=" min-h-[200px] py-4 sm:w-[28rem] sm:px-5 sm:col-span-4">
-            <img src={aboutChair} alt="aboutChairImg" className="sm:w-full" />
+            <img src={aboutChair} alt="aboutChairImg" className="sm:w-full w-[90%]" />
           </div>
-          <div className="animation-div-right"></div>
+          <div className="animation-div-right "></div>
           <div className="animation-div-home-about"></div>
         </div>
       </div>
       {/* services page */}
       <div
-        className="h-[100vh] flex justify-center overflow-x-hidden"
-        style={{ justifyContent: "center" }}
-      >
-        {/* <img
+      className="h-[110vh] flex justify-center overflow-x-hidden"
+      style={{ justifyContent: "center" }}
+    >
+
+      {/* <img
         src={servicesBg}
         alt=" home-page-img"
         className=" bg-cover sm:h-[120vh] w-full "
       /> */}
-        {/* <div className="animation-div"></div> */}
-        <div
-          className=" absolute h-full grid items-center  bg-white sm:grid-cols-10"
-          style={{ justifyItems: "center" }}
-        >
-          <div
-            className="sm:col-span-4 flex flex-col items-start"
-            style={{ paddingLeft: "2rem" }}
+      {/* <div className="animation-div block"></div> */}
+      <div
+        className=" absolute h-full grid items-center  bg-white sm:grid-cols-10"
+        style={{ justifyItems: "center" }}
+      >
+        <div className="sm:col-span-4 flex flex-col items-start" style={{ paddingLeft: "2rem" }}>
+          <h1 className="font-bold sm:text-8xl text-3xl sm:p-5 p-2 self-start">
+            MODERN FURNITURE
+          </h1>
+          <h2 className="collection sm:text-8xl text-2xl">Collections</h2>
+          <p className="sm:text-xl text-sm p-4  text-black">
+            Explore our carefully curated modern furniture collection designed
+            to elevate your living spaces. Discover a blend of sleek designs,
+            premium materials, and timeless craftsmanship that perfectly
+            balances aesthetics and functionality.
+          </p>
+          <button
+            class="relative m-[5px] inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800"
+            style={{ marginLeft: "15px" }}
           >
-            <h1 className="font-bold sm:text-8xl text-3xl sm:p-5 p-2 self-start">
-              MODERN FURNITURE
-            </h1>
-            <h2 className="collection sm:text-8xl text-2xl">Collections</h2>
-            <p className="sm:text-xl text-sm p-4  text-black">
-              Explore our carefully curated modern furniture collection designed
-              to elevate your living spaces. Discover a blend of sleek designs,
-              premium materials, and timeless craftsmanship that perfectly
-              balances aesthetics and functionality.
-            </p>
-            <button
-              class="relative m-[5px] inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800"
-              style={{ marginLeft: "15px" }}
-            >
-              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-700 rounded-md uppercase group-hover:bg-opacity-0">
-                buy now
-              </span>
-            </button>
-            <div className="animation-div"></div>
-          </div>
-          <div className="sm:col-span-2"></div>
-          <div className=" sm:w-[60vw] sm:px-5 sm:col-span-4">
-            <img src={servicesInfo} alt="servicesInfo" className="" />
-            <div className="animation-div"></div>
-          </div>
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-700 rounded-md uppercase group-hover:bg-opacity-0">
+              buy now
+            </span>
+          </button>
+          <div className="animation-div"></div>
+        </div>
+        <div className="sm:col-span-2"></div>
+        <div className=" sm:w-[60vw] sm:px-5 sm:col-span-4">
+          <img
+            src={servicesInfo}
+            alt="servicesInfo"
+            className="w-[90%] z-[-1] sm:h-[80%]"
+          />
+          {/* <div className="animation-div"></div> */}
         </div>
       </div>
+    </div>
     </>
   );
 };
